75cf728f442d7f52fb64b5e2c5728e6d78e6f710
Roudeina <roudeina.bentrad@gmail.com>
2021-03-21 14:13:43 +0100

    v1

---
+++ main.js
@@ -0,0 +1,59 @@
+const addButton = document.querySelector(".addButton");
+var input = document.querySelector(".input");
+const container = document.querySelector(".container");
+
+function item(itemName) {
+  function createDiv(itemName) {
+    var input = document.createElement("input"); 
+    input.value = itemName;
+    input.disabled = true;
+    input.type = "text";
+    input.classList.add("item_input"); 
+
+    var itemBox = document.createElement("div");
+    itemBox.classList.add("item");
+
+    var editButton = document.createElement("button"); 
+    editButton.innerHTML = "EDIT";
+    editButton.classList.add("editButton");
+
+    var removeButton = document.createElement("button");
+    removeButton.innerHTML = "REMOVE";
+    removeButton.classList.add("removeButton");
+
+
+    container.appendChild(itemBox);
+    itemBox.appendChild(input);
+    itemBox.appendChild(editButton);
+    itemBox.appendChild(removeButton);
+
+    editButton.addEventListener("click", () => edit(input)); 
+    removeButton.addEventListener("click", () => remove(itemBox)); 
+
+  }
+
+  function edit(input) {
+    input.disabled = !input.disabled;
+  }
+
+  function remove(itemBox) {
+    container.removeChild(itemBox);
+  }
+
+  createDiv(itemName);
+}
+
+function check() {
+  if (input.value !== "") {
+    new item(input.value);
+    input.value = "";
+  }
+}
+
+addButton.addEventListener("click", check);
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    check();
+  }
+});
